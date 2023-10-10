cc6f4e64ecd9908cb9588b329fb195bc5acc4115
Thanh-PH20122 <thanhdvph20122@pft.edu.vn>
2023-10-10 15:32:53 +0700

    xong chức năng đăng nhập sdt

--- lib/Provider/dang_nhap_sdt_provider.dart
+++ lib/Provider/dang_nhap_sdt_provider.dart
@@ -1,15 +1,81 @@
+import 'package:app/View/Screen/DangNhap/man_hinh_otp.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class DangNhapSdtProvider extends ChangeNotifier{
+  bool isPhoneNumberCheck= false;
+  final FirebaseAuth auth = FirebaseAuth.instance;
   bool isChecked = false;
   String phone = '';
+  String error = '';
+  bool isLoading = false;
+  String verificationId = '';
+  bool isCheckOtp = false;
+
   void changeChecked(bool check){
     isChecked = check;
+    notifyListeners();
+  }
+
+
+  void changCheckOTP(bool check){
+    isCheckOtp = check;
     notifyListeners();
   }
 
   void changePhone(String phone){
     this.phone = phone;
     notifyListeners();
+  }
+
+  void changeLoading(bool loading){
+    this.isLoading = loading;
+    notifyListeners();
+  }
+
+  void changePhoneNumberCheck(bool isPhoneNumberCheck){
+    this.isPhoneNumberCheck = isPhoneNumberCheck;
+    notifyListeners();
+  }
+
+  Future<void> dangNhapPhone(BuildContext context) async {
+    changeLoading(true);
+    await FirebaseAuth.instance.verifyPhoneNumber(
+      phoneNumber: '+84$phone',
+      verificationCompleted: (PhoneAuthCredential credential) { },
+      verificationFailed: (FirebaseAuthException e) {
+        changePhoneNumberCheck(true);
+        changeLoading(false);
+      },
+      codeSent: (String verificationId, int? resendToken) {
+        this.verificationId = verificationId;
+        changeLoading(false);
+        Navigator.push(context,
+          MaterialPageRoute(builder: (context) => ManHinhOTP())
+        );
+      },
+      codeAutoRetrievalTimeout: (String verificationId) {},
+    );
+  }
+
+  void verifyOTP(BuildContext context,String otp) async {
+    try {
+      changeLoading(true);
+      // Tạo một PhoneAuthCredential từ OTP và verificationId
+      PhoneAuthCredential credential = PhoneAuthProvider.credential(
+        verificationId: verificationId,
+        smsCode: otp,
+      );
+      // Xác minh OTP và đăng nhập người dùng
+      await FirebaseAuth.instance.signInWithCredential(credential);
+      print('thành công');
+      changCheckOTP(false);
+      changeLoading(false);
+      // Đăng nhập thành công, bạn có thể thực hiện các hành động sau đây.
+    } catch (e) {
+      changeLoading(false);
+      changCheckOTP(true);
+      print('Xác minh OTP thất bại: $e');
+    }
   }
 }
--- lib/View/Screen/DangNhap/man_hinh_otp.dart
+++ lib/View/Screen/DangNhap/man_hinh_otp.dart
@@ -4,6 +4,8 @@ import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
 import 'package:provider/provider.dart';
 import 'dart:async';
 
+import '../../Widget/loading.dart';
+
 class ManHinhOTP extends StatefulWidget {
   @override
   _ManHinhOTPState createState() => _ManHinhOTPState();
@@ -13,7 +15,6 @@ class _ManHinhOTPState extends State<ManHinhOTP> {
   int _secondsRemaining = 60; // Số giây còn lại
   late Timer _timer; // Đối tượng Timer để đếm ngược
   bool _isCodeSubmitted = true;
-  bool _isCheckOtp = false;
 
   @override
   void initState() {
@@ -25,7 +26,7 @@ class _ManHinhOTPState extends State<ManHinhOTP> {
     const oneSecond = Duration(seconds: 1);
     _timer = Timer.periodic(
       oneSecond,
-          (timer) {
+      (timer) {
         setState(() {
           if (_secondsRemaining > 0) {
             _secondsRemaining--;
@@ -45,119 +46,125 @@ class _ManHinhOTPState extends State<ManHinhOTP> {
 
   @override
   Widget build(BuildContext context) {
-    DangNhapSdtProvider provider = context.read<DangNhapSdtProvider>();
-    return Container(
-      color: Colors.white,
-      child: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.white,
-            leading: IconButton(
-              icon: const Icon(
-                Icons.arrow_back,
-                color: Colors.black,
-              ),
-              onPressed: () {
-                Navigator.of(context).pop(); // Để quay lại màn hình trước đó
-              },
-            ),
-          ),
-          body: Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Nhập mã gồm 6 chữ số',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  'Mã của bạn đã được gửi đến +84${provider.phone}',
-                  style: const TextStyle(
-                    color: Colors.grey,
-                    fontSize: 14,
-                  ),
-                ),
-                OtpTextField(
-                  numberOfFields: 6,
-                  borderColor: Color(0xFF512DA8),
-                  cursorColor: Colors.pinkAccent,
-                  enabled: _isCodeSubmitted,
-                  focusedBorderColor: Colors.pinkAccent,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  autoFocus: true,
-                  onCodeChanged: (String code) {
-                    // Xử lý sự kiện khi người dùng thay đổi mã
-                  },
-                  onSubmit: (String verificationCode) {
-                    setState(() {
-                      _isCodeSubmitted = false;
-                      showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            title: Text("Verification Code"),
-                            content: Text('Code entered is $verificationCode'),
-                          );
-                        },
-                      );
-                    });
-
-                  },
-                ),
-                SizedBox(height: 20,),
-                _isCheckOtp ? Text(
-                    'Mã xác minh sai vui lòng thử lại !',
-                    style: TextStyle(
-                      color: Colors.red
-                    ),
-                ) : SizedBox(),
-                const SizedBox(height: 10),
-                Row(
-                  children: [
-                    TextButton(
-                      onPressed: _secondsRemaining > 0 ? null
-                          :(){
-                        setState(() {
-                          _isCodeSubmitted = true;
-                          _secondsRemaining = 60;
-                          startTimer();
-                        });
+    return Consumer<DangNhapSdtProvider>(
+      builder: (context, provider, _) {
+        return Stack(
+          children: [
+            Container(
+              color: Colors.white,
+              child: SafeArea(
+                child: Scaffold(
+                  appBar: AppBar(
+                    backgroundColor: Colors.white,
+                    leading: IconButton(
+                      icon: const Icon(
+                        Icons.arrow_back,
+                        color: Colors.black,
+                      ),
+                      onPressed: () {
+                        Navigator.of(context)
+                            .pop(); // Để quay lại màn hình trước đó
                       },
-                      child: Text(
-                          'Gửi lại mã',
+                    ),
+                  ),
+                  body: Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Nhập mã gồm 6 chữ số',
                           style: TextStyle(
-                              color: _secondsRemaining > 0
-                                  ? Colors.grey : Colors.pinkAccent,
-                              fontWeight: FontWeight.bold
+                            color: Colors.black,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
                           ),
-                      ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Mã của bạn đã được gửi đến +84${provider.phone}',
+                          style: const TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14,
+                          ),
+                        ),
+                        OtpTextField(
+                          numberOfFields: 6,
+                          borderColor: Color(0xFF512DA8),
+                          cursorColor: Colors.pinkAccent,
+                          enabled: _isCodeSubmitted,
+                          focusedBorderColor: Colors.pinkAccent,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          autoFocus: true,
+                          onCodeChanged: (String code) {
+                            // Xử lý sự kiện khi người dùng thay đổi mã
+                          },
+                          onSubmit: (String verificationCode) {
+                            setState(() {
+                              _isCodeSubmitted = false;
+                              provider.verifyOTP(context,verificationCode);
+                              _isCodeSubmitted = true;
+                            });
+                          },
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        provider.isCheckOtp
+                            ? Text(
+                                'Mã xác minh sai vui lòng thử lại !',
+                                style: TextStyle(color: Colors.red),
+                              )
+                            : SizedBox(),
+                        const SizedBox(height: 10),
+                        Row(
+                          children: [
+                            TextButton(
+                              onPressed: _secondsRemaining > 0
+                                  ? null
+                                  : () {
+                                      setState(() {
+                                        _isCodeSubmitted = true;
+                                        _secondsRemaining = 60;
+                                        startTimer();
+                                      });
+                                    },
+                              child: Text(
+                                'Gửi lại mã',
+                                style: TextStyle(
+                                    color: _secondsRemaining > 0
+                                        ? Colors.grey
+                                        : Colors.pinkAccent,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              '$_secondsRemaining s',
+                              style: TextStyle(
+                                  color: _secondsRemaining > 0
+                                      ? Colors.grey
+                                      : Colors.grey,
+                                  // Thay đổi màu sắc khi hết thời gian
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                    SizedBox(width: 10,),
-                    Text(
-                      '$_secondsRemaining s',
-                      style: TextStyle(
-                        color: _secondsRemaining > 0
-                            ? Colors.grey
-                            : Colors.grey, // Thay đổi màu sắc khi hết thời gian
-                        fontWeight: FontWeight.bold
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              ],
+              ),
             ),
-          ),
-        ),
-      ),
+            LoadingWidget(isLoading: provider.isLoading),
+          ],
+        );
+      },
     );
   }
 }
