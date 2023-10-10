c9455b0b61258a84245886fe0f0cae3bc8e564b8
Thanh-PH20122 <thanhdvph20122@pft.edu.vn>
2023-10-10 09:23:48 +0700

    sua 2 giao dien dang nhap, dang ky

--- lib/View/Screen/DangNhap/man_hinh_dang_nhap.dart
+++ lib/View/Screen/DangNhap/man_hinh_dang_nhap.dart
@@ -2,31 +2,65 @@ import 'package:app/View/Screen/DangKy/man_hinh_dang_ky.dart';
 import 'package:app/View/Widget/button.dart';
 import 'package:flutter/material.dart';
 
-class ManHinhDangNhap extends StatelessWidget{
+class ManHinhDangNhap extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.only(left: 30,top: 50),
+        margin: EdgeInsets.only(top: 50),
         child: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Texts("Đăng nhập TikTok",30,FontWeight.w800),
+              Texts("Đăng nhập TikTok", 30, FontWeight.w800),
               SizedBox(height: 10),
-              Texts("Quản lý tài khoản, kiếm tra thông báo, bình\n luận trên các video,v.v ", 18, FontWeight.normal),
+              Texts(
+                  "Quản lý tài khoản, kiếm tra thông báo, bình\n luận trên các video,v.v ",
+                  18,
+                  FontWeight.normal),
               SizedBox(height: 40),
-              ButtonCusstom(text: "Đăng nhập bằng số điện thoại", icon: Icons.phone, onPress: (){}),
+              ButtonCusstom(
+                  text: "Đăng nhập bằng số điện thoại & Email",
+                  icon: Icons.person,
+                  onPress: () {}),
               SizedBox(height: 40),
-              ButtonCusstom(text: "Đăng nhập bằng email", icon: Icons.email, onPress: (){}),
+              ButtonCusstom(
+                  text: "Tiếp tục với Google",
+                  icon: Icons.g_mobiledata_sharp,
+                  onPress: () {}),
               SizedBox(height: 40),
-              ButtonCusstom(text: "Tiếp tục với Google", icon: Icons.g_mobiledata_sharp, onPress: (){}),
-              SizedBox(height: 40),
-              ButtonCusstom(text: "Tiếp tục với facebook", icon: Icons.facebook, onPress: (){}),
+              ButtonCusstom(
+                  text: "Tiếp tục với facebook",
+                  icon: Icons.facebook,
+                  onPress: () {}),
               SizedBox(height: 100),
-              textButton("Bạn không có tài khoản?", 'Đăng ký', 18, FontWeight.bold,(){
+              textButton(
+                  "Bạn không có tài khoản?", 'Đăng ký', 18, FontWeight.bold,
+                  () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => ManHinhDangKy()),
+                  PageRouteBuilder(
+                    pageBuilder: (context, animation, secondaryAnimation) =>
+                        ManHinhDangKy(),
+                    transitionsBuilder:
+                        (context, animation, secondaryAnimation, child) {
+                      const begin = Offset(1.0,
+                          0.0); // Điểm bắt đầu (nếu bạn muốn chuyển từ bên phải)
+                      const end = Offset
+                          .zero; // Điểm kết thúc (nếu bạn muốn hiển thị ở giữa)
+                      const curve = Curves
+                          .easeInOut; // Loại chuyển cảnh (có thể tùy chỉnh)
+
+                      var tween = Tween(begin: begin, end: end)
+                          .chain(CurveTween(curve: curve));
+                      var offsetAnimation = animation.drive(tween);
+
+                      return SlideTransition(
+                        position: offsetAnimation,
+                        child: child,
+                      );
+                    },
+                  ),
                 );
               }),
             ],
@@ -35,7 +69,8 @@ class ManHinhDangNhap extends StatelessWidget{
       ),
     );
   }
-  Widget Texts(String lable,double size,FontWeight fontWeight){
+
+  Widget Texts(String lable, double size, FontWeight fontWeight) {
     return Text(
       lable,
       style: TextStyle(
@@ -45,7 +80,9 @@ class ManHinhDangNhap extends StatelessWidget{
       textAlign: TextAlign.center,
     );
   }
-  Widget textButton(String text,String buttonText,double size,FontWeight fontWeight,VoidCallback onPress){
+
+  Widget textButton(String text, String buttonText, double size,
+      FontWeight fontWeight, VoidCallback onPress) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -58,14 +95,11 @@ class ManHinhDangNhap extends StatelessWidget{
         ),
         SizedBox(width: 5),
         TextButton(
-          onPressed:  onPress,
+          onPressed: onPress,
           child: Text(
             buttonText,
             style: TextStyle(
-                fontSize: size,
-                fontWeight: fontWeight,
-                color: Colors.red
-            ),
+                fontSize: size, fontWeight: fontWeight, color: Colors.red),
           ),
         )
       ],
