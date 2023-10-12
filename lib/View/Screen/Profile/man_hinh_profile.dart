880aa079fbce08268ebfb02b20ddca5840d75d61
demo <duongdtph27826@fpt.edu.vn>
2023-10-12 15:07:41 +0700

    avatar test

--- lib/View/Screen/Profile/man_hinh_profile.dart
+++ lib/View/Screen/Profile/man_hinh_profile.dart
@@ -1,4 +1,3 @@
-
 import 'package:app/View/Screen/Profile/main_hinh_editProfile.dart';
 import 'package:app/View/Screen/Profile/tab_bookmark.dart';
 import 'package:app/View/Screen/Profile/tab_video.dart';
@@ -16,11 +15,14 @@ class ManHinhProfile extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            Avatar('https://cdn.pixabay.com/photo/2016/11/14/04/36/boy-1822614_640.jpg'),
+            Avatar(
+                'https://cdn.pixabay.com/photo/2016/11/14/04/36/boy-1822614_640.jpg',
+                context
+            ),
             SizedBox(height: 20),
             text(lable: 'Username', size: 18, fontWeight: FontWeight.normal),
             SizedBox(height: 20),
-            TrangThai(10,5,5),
+            TrangThai(10, 5, 5),
             SizedBox(height: 30),
             textButton(context),
             SizedBox(height: 10),
@@ -31,15 +33,19 @@ class ManHinhProfile extends StatelessWidget {
     );
   }
 
-  Avatar(String url) {
+  Avatar(String url, BuildContext sContext) {
     return Container(
       child: Column(
         children: [
           SizedBox(height: 40),
-            Stack(
-              alignment: Alignment.bottomRight,
-              children: [
-                ClipOval(
+          Stack(
+            alignment: Alignment.bottomRight,
+            children: [
+              ClipOval(
+                child: GestureDetector(
+                  onTap: () {
+                    showAboutDialog(context: sContext);
+                  },
                   child: CachedNetworkImage(
                     imageUrl: url,
                     fit: BoxFit.cover,
@@ -47,17 +53,19 @@ class ManHinhProfile extends StatelessWidget {
                     width: 100,
                   ),
                 ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.upload, color: Colors.redAccent),
-                ),
-              ],
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.upload, color: Colors.redAccent),
+              ),
+            ],
           )
         ],
       ),
     );
   }
-  TrangThai(int dangFollow, int follow,int like){
+
+  TrangThai(int dangFollow, int follow, int like) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
@@ -65,27 +73,36 @@ class ManHinhProfile extends StatelessWidget {
           child: Container(
             child: Column(
               children: [
-                text(lable: dangFollow.toString(), size: 20, fontWeight: FontWeight.w900),
+                text(lable: dangFollow.toString(),
+                    size: 20,
+                    fontWeight: FontWeight.w900),
                 SizedBox(height: 5),
-                text(lable: "Đang follow", size: 15, fontWeight: FontWeight.normal),
+                text(lable: "Đang follow",
+                    size: 15,
+                    fontWeight: FontWeight.normal),
               ],
             ),
           ),
-        ),Expanded(
+        ), Expanded(
           child: Container(
             child: Column(
               children: [
-                text(lable: follow.toString(), size: 20, fontWeight: FontWeight.w900),
+                text(lable: follow.toString(),
+                    size: 20,
+                    fontWeight: FontWeight.w900),
                 SizedBox(height: 5),
-                text(lable: "Follower", size: 15, fontWeight: FontWeight.normal),
+                text(
+                    lable: "Follower", size: 15, fontWeight: FontWeight.normal),
               ],
             ),
           ),
-        ),Expanded(
+        ), Expanded(
           child: Container(
             child: Column(
               children: [
-                text(lable: like.toString(), size: 20, fontWeight: FontWeight.w900),
+                text(lable: like.toString(),
+                    size: 20,
+                    fontWeight: FontWeight.w900),
                 SizedBox(height: 5),
                 text(lable: "Thích", size: 15, fontWeight: FontWeight.normal),
               ],
@@ -95,95 +112,101 @@ class ManHinhProfile extends StatelessWidget {
       ],
     );
   }
-  textButton(BuildContext context){
+
+  textButton(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         OutlinedButton(
-            style: ButtonStyle(
-              padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                EdgeInsets.all(20.0), // Đặt giá trị padding là 10
-              ),
-              minimumSize: MaterialStateProperty.all<Size>(
-                Size(100, 40), // Đặt kích thước theo chiều rộng và chiều cao mong muốn
-              ),
+          style: ButtonStyle(
+            padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+              EdgeInsets.all(20.0), // Đặt giá trị padding là 10
             ),
-            onPressed: (){
-              Navigator.push(
-                context,
-                PageRouteBuilder(
-                  pageBuilder: (context, animation, secondaryAnimation) =>
-                      EditProfile(),
-                  transitionsBuilder:
-                      (context, animation, secondaryAnimation, child) {
-                    const begin = Offset(0.0, 1.0); // Điểm bắt đầu (nếu bạn muốn chuyển từ bên phải)
-                    const end = Offset
-                        .zero; // Điểm kết thúc (nếu bạn muốn hiển thị ở giữa)
-                    const curve = Curves
-                        .easeInOut; // Loại chuyển cảnh (có thể tùy chỉnh)
-                    var tween = Tween(begin: begin, end: end)
-                        .chain(CurveTween(curve: curve));
-                    var offsetAnimation = animation.drive(tween);
-                    return SlideTransition(
-                      position: offsetAnimation,
-                      child: child,
-                    );
-                  },
-                ),
-              );
-            },
-            child: Text(
-              'Sửa hồ sơ',
-              style: TextStyle(
+            minimumSize: MaterialStateProperty.all<Size>(
+              Size(100,
+                  40), // Đặt kích thước theo chiều rộng và chiều cao mong muốn
+            ),
+          ),
+          onPressed: () {
+            Navigator.push(
+              context,
+              PageRouteBuilder(
+                pageBuilder: (context, animation, secondaryAnimation) =>
+                    EditProfile(),
+                transitionsBuilder:
+                    (context, animation, secondaryAnimation, child) {
+                  const begin = Offset(0.0,
+                      1.0); // Điểm bắt đầu (nếu bạn muốn chuyển từ bên phải)
+                  const end = Offset
+                      .zero; // Điểm kết thúc (nếu bạn muốn hiển thị ở giữa)
+                  const curve = Curves
+                      .easeInOut; // Loại chuyển cảnh (có thể tùy chỉnh)
+                  var tween = Tween(begin: begin, end: end)
+                      .chain(CurveTween(curve: curve));
+                  var offsetAnimation = animation.drive(tween);
+                  return SlideTransition(
+                    position: offsetAnimation,
+                    child: child,
+                  );
+                },
+              ),
+            );
+          },
+          child: Text(
+            'Sửa hồ sơ',
+            style: TextStyle(
                 color: Colors.black
-              ),
             ),
+          ),
         ),
         SizedBox(width: 20),
         OutlinedButton(
-            style: ButtonStyle(
-              padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                EdgeInsets.all(20.0), // Đặt giá trị padding là 10
-              ),
-              minimumSize: MaterialStateProperty.all<Size>(
-                Size(100, 40), // Đặt kích thước theo chiều rộng và chiều cao mong muốn
-              ),
+          style: ButtonStyle(
+            padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+              EdgeInsets.all(20.0), // Đặt giá trị padding là 10
             ),
-            onPressed: (){
-              Navigator.push(
-                context,
-                PageRouteBuilder(
-                  pageBuilder: (context, animation, secondaryAnimation) =>
-                      AddFriend(),
-                  transitionsBuilder:
-                      (context, animation, secondaryAnimation, child) {
-                    const begin = Offset(0.0, 1.0); // Điểm bắt đầu (nếu bạn muốn chuyển từ bên phải)
-                    const end = Offset
-                        .zero; // Điểm kết thúc (nếu bạn muốn hiển thị ở giữa)
-                    const curve = Curves
-                        .easeInOut; // Loại chuyển cảnh (có thể tùy chỉnh)
-                    var tween = Tween(begin: begin, end: end)
-                        .chain(CurveTween(curve: curve));
-                    var offsetAnimation = animation.drive(tween);
-                    return SlideTransition(
-                      position: offsetAnimation,
-                      child: child,
-                    );
-                  },
-                ),
-              );
-            },
-            child: Text(
-              'Thêm bạn',
-              style: TextStyle(
+            minimumSize: MaterialStateProperty.all<Size>(
+              Size(100,
+                  40), // Đặt kích thước theo chiều rộng và chiều cao mong muốn
+            ),
+          ),
+          onPressed: () {
+            Navigator.push(
+              context,
+              PageRouteBuilder(
+                pageBuilder: (context, animation, secondaryAnimation) =>
+                    AddFriend(),
+                transitionsBuilder:
+                    (context, animation, secondaryAnimation, child) {
+                  const begin = Offset(0.0,
+                      1.0); // Điểm bắt đầu (nếu bạn muốn chuyển từ bên phải)
+                  const end = Offset
+                      .zero; // Điểm kết thúc (nếu bạn muốn hiển thị ở giữa)
+                  const curve = Curves
+                      .easeInOut; // Loại chuyển cảnh (có thể tùy chỉnh)
+                  var tween = Tween(begin: begin, end: end)
+                      .chain(CurveTween(curve: curve));
+                  var offsetAnimation = animation.drive(tween);
+                  return SlideTransition(
+                    position: offsetAnimation,
+                    child: child,
+                  );
+                },
+              ),
+            );
+          },
+          child: Text(
+            'Thêm bạn',
+            style: TextStyle(
                 color: Colors.black
-              ),
             ),
+          ),
         ),
       ],
     );
   }
-  TastBar(){
+
+  TastBar() {
     return DefaultTabController(
         length: 2,
         child: Scaffold(
@@ -199,24 +222,39 @@ class ManHinhProfile extends StatelessWidget {
                   ],
                 ),
               ),
-
               Positioned(
                 top: 0,
-                  right: 0,
-                  left: 0,
-                  child: TabBar(
-                    indicatorColor: Colors.black,
-                    tabs: [
-                      Tab(child: Icon(Icons.video_collection,color: Colors.black,)),
-                      Tab(child: Icon(Icons.bookmark,color: Colors.black,)),
-                    ],
-                    labelPadding: EdgeInsets.symmetric(horizontal: 0),
-              ),
+                right: 0,
+                left: 0,
+                child: TabBar(
+                  indicatorColor: Colors.black,
+                  tabs: [
+                    Tab(child: Icon(
+                      Icons.video_collection, color: Colors.black,)),
+                    Tab(child: Icon(Icons.bookmark, color: Colors.black,)),
+                  ],
+                  labelPadding: EdgeInsets.symmetric(horizontal: 0),
+                ),
               ),
 
             ],
           ),
         )
+    );
+  }
+
+  showAvatarDialog(BuildContext context) {
+    return showModalBottomSheet(
+        context: context,
+        builder: (context){
+          return Container(
+            child: Column(
+              children: [
+                Text("data"),
+              ],
+            ),
+          );
+        }
     );
   }
 
