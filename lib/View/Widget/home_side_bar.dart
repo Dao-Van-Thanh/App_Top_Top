5cbb63f4266c3db476d28da0b86e98c4de27c869
TungDuong <duongdtph27826@fpt.edu.vn>
2023-10-17 20:32:35 +0700

    fix lỗi hiển thị textInput

--- lib/View/Widget/home_side_bar.dart
+++ lib/View/Widget/home_side_bar.dart
@@ -74,10 +74,7 @@ class HomeSideBar extends StatelessWidget {
                 showModalBottomSheet(
                   context: context,
                   builder: (context) {
-                    return Provider<EmojiProvider>(
-                      create: (_) => EmojiProvider(),
-                      child: CommentsDialog(videoProvider.videoId),
-                    );
+                    return  CommentsDialog(videoId:videoProvider.videoId);
                   },
                 );
                 break;
