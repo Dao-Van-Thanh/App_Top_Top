0c14ac344be3cecc20485c45a0e428c31fa67b19
Thanh-PH20122 <thanhdvph20122@pft.edu.vn>
2023-10-23 15:56:31 +0700

    thêm chức năng status của user

--- lib/View/Widget/bottom_navigation.dart
+++ lib/View/Widget/bottom_navigation.dart
@@ -1,22 +1,14 @@
 
 import 'package:app/Provider/page_provider.dart';
-import 'package:app/View/Pages/Chats/man_hinh_chat.dart';
-
 import 'package:app/Services/user_service.dart';
 import 'package:app/View/Pages/Chats/man_hinh_hop_thu.dart';
-
 import 'package:app/View/Pages/Profile/man_hinh_profile.dart';
 import 'package:app/View/Pages/QuayVideo/man_hinh_quay_video.dart';
 import 'package:app/View/Widget/custom_icon_add_video.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../../Provider/edit_item_profile_provider.dart';
-import '../Pages/TrangChu/trang_chu.dart';
-import 'package:app/View/Pages/TrangChu/trang_chu.dart';
-import 'package:app/View/Widget/custom_icon_add_video.dart';
-import 'package:flutter/material.dart';
 
-import '../Pages/QuayVideo/man_hinh_quay_video.dart';
+import '../Pages/TrangChu/trang_chu.dart';
 
 
 class Bottom_Navigation_Bar extends StatefulWidget {
