1fd0616fc13e4e7f0145468a0a653b2575374628
Thanh-PH20122 <thanhdvph20122@pft.edu.vn>
2023-10-23 14:50:51 +0700

    thêm chức năng status của user

--- lib/Provider/chats_provider.dart
+++ lib/Provider/chats_provider.dart
@@ -3,6 +3,11 @@ import 'package:flutter/cupertino.dart';
 
 class ChatsProfiver extends ChangeNotifier{
     int check = 0;
+    bool emojiShowing = false;
+    void setEmojiShowing(bool check){
+      emojiShowing = check;
+      notifyListeners();
+    }
     void setCheck(int i){
       check = i;
       notifyListeners();
