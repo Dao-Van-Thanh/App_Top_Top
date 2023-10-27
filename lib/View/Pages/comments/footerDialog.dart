35af6b9dda10c8c869b61ac37b46f1a2c633d616
Thanh-PH20122 <thanhdvph20122@pft.edu.vn>
2023-10-27 15:50:00 +0700

    thêm chức năng EmojiPicker vào comments

--- lib/View/Pages/comments/footerDialog.dart
+++ lib/View/Pages/comments/footerDialog.dart
@@ -1,13 +1,18 @@
+import 'package:app/Provider/chats_provider.dart';
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../../Provider/comments_provider.dart';
 import '../../../Provider/video_provider.dart';
 import '../../../Services/comment_service.dart';
+import 'package:flutter/foundation.dart' as foundation;
 
 class FooterDialog extends StatefulWidget {
   final String? avatarURL;
   final String videoId;
   final String? uId;
+
   // final VideoProvider videoProvider;
 
   FooterDialog(
@@ -27,6 +32,8 @@ class _FooterDialogState extends State<FooterDialog> {
   @override
   void initState() {
     super.initState();
+    final provider = Provider.of<ChatsProfiver>(context,listen: false);
+    provider.emojiShowing = false;
     myFocusNode.requestFocus();
   }
 
@@ -36,68 +43,134 @@ class _FooterDialogState extends State<FooterDialog> {
     bool check = setComment.reline;
     TextEditingController textEditingController = TextEditingController();
     return Container(
-      width: MediaQuery.of(context).size.width,
-      color: Colors.white,
-      padding: const EdgeInsets.all(10),
-      child: Row(
-        children: [
-          if (widget.avatarURL != null)
-            CircleAvatar(
-              backgroundColor: Colors.black,
-              backgroundImage: NetworkImage(
-                widget.avatarURL!,
-              ),
-            )
-          else
-            const CircularProgressIndicator(),
-          const SizedBox(width: 5),
-          Expanded(
-            child: Card(
-              color: Colors.white70,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding:
-                          const EdgeInsets.only(left: 20, top: 5, bottom: 5),
-                      child: TextField(
-                        focusNode: myFocusNode,
-                        maxLines: null,
-                        controller: textEditingController,
-                        decoration: const InputDecoration(
-                          hintText: 'Thêm bình luận...',
-                          border: InputBorder.none,
+        width: MediaQuery.of(context).size.width,
+        color: Colors.white,
+        child: Consumer<ChatsProfiver>(
+          builder: (context, provider, child) {
+            return Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    children: [
+                      if (widget.avatarURL != null)
+                        CircleAvatar(
+                          backgroundColor: Colors.black,
+                          backgroundImage: NetworkImage(
+                            widget.avatarURL!,
+                          ),
+                        )
+                      else
+                        const CircularProgressIndicator(),
+                      const SizedBox(width: 5),
+                      Expanded(
+                        child: Card(
+                          color: Colors.white70,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 20, top: 5, bottom: 5),
+                                  child: TextField(
+                                    focusNode: myFocusNode,
+                                    maxLines: null,
+                                    controller: textEditingController,
+                                    decoration: const InputDecoration(
+                                      hintText: 'Thêm bình luận...',
+                                      border: InputBorder.none,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  if(provider.emojiShowing){
+                                    myFocusNode.requestFocus();
+                                  }else{
+                                    FocusScope.of(context).requestFocus(FocusNode());
+                                  }
+                                  provider.setEmojiShowing(!provider.emojiShowing);
+                                },
+                                icon: const Icon(Icons.emoji_emotions),
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  CommentService().sendComment(widget.videoId,
+                                      textEditingController.text.trim(), widget.uId!);
+                                  textEditingController.clear();
+                                  Navigator.pop(context);
+                                  // int index = videoProvider.listVideo
+                                  //     .indexWhere((element) => element == videoId);
+                                  // videoProvider.listVideo[index].comments.add('');
+                                },
+                                icon: Icon(Icons.send),
+                              ),
+                            ],
+                          ),
                         ),
+                      ),
+                    ],
+                  ),
+                ),
+                Offstage(
+                  offstage: !provider.emojiShowing,
+                  child: SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.3,
+                    child: EmojiPicker(
+                      // onEmojiSelected: (Category category, Emoji emoji) {
+                      //   // Do something when emoji is tapped (optional)
+                      // },
+                      onBackspacePressed: () {
+                        // Do something when the user taps the backspace button (optional)
+                        // Set it to null to hide the Backspace-Button
+                      },
+                      textEditingController: textEditingController,
+                      // pass here the same [TextEditingController] that is connected to your input field, usually a [TextFormField]
+                      config: Config(
+                        columns: 7,
+                        emojiSizeMax: 32 *
+                            (foundation.defaultTargetPlatform ==
+                                TargetPlatform.iOS
+                                ? 1.30
+                                : 1.0),
+                        // Issue: https://github.com/flutter/flutter/issues/28894
+                        verticalSpacing: 0,
+                        horizontalSpacing: 0,
+                        gridPadding: EdgeInsets.zero,
+                        initCategory: Category.RECENT,
+                        bgColor: Color(0xFFF2F2F2),
+                        indicatorColor: Colors.blue,
+                        iconColor: Colors.grey,
+                        iconColorSelected: Colors.blue,
+                        backspaceColor: Colors.blue,
+                        skinToneDialogBgColor: Colors.white,
+                        skinToneIndicatorColor: Colors.grey,
+                        enableSkinTones: true,
+                        recentTabBehavior: RecentTabBehavior.RECENT,
+                        recentsLimit: 28,
+                        noRecents: const Text(
+                          'No Recents',
+                          style: TextStyle(
+                              fontSize: 20, color: Colors.black26),
+                          textAlign: TextAlign.center,
+                        ),
+                        // Needs to be const Widget
+                        loadingIndicator: const SizedBox.shrink(),
+                        // Needs to be const Widget
+                        tabIndicatorAnimDuration: kTabScrollDuration,
+                        categoryIcons: const CategoryIcons(),
+                        buttonMode: ButtonMode.MATERIAL,
                       ),
                     ),
                   ),
-                  IconButton(
-                    onPressed: () {
-                      // Handle emoji picker here.
-                    },
-                    icon: const Icon(Icons.emoji_emotions),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      CommentService()
-                          .sendComment(widget.videoId, textEditingController.text.trim(), widget.uId!);
-                      textEditingController.clear();
-                      Navigator.pop(context);
-                      // int index = videoProvider.listVideo
-                      //     .indexWhere((element) => element == videoId);
-                      // videoProvider.listVideo[index].comments.add('');
-                    },
-                    icon: Icon(Icons.send),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+                ),
+              ],
+            );
+          },
+        ));
   }
 }
--- lib/View/Pages/comments/footer_dialog_recomment.dart
+++ lib/View/Pages/comments/footer_dialog_recomment.dart
@@ -1,7 +1,8 @@
+import 'package:app/Provider/chats_provider.dart';
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
-
-import '../../../Provider/comments_provider.dart';
-import '../../../Provider/video_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/foundation.dart' as foundation;
 import '../../../Services/comment_service.dart';
 
 class FooterDialogReComment extends StatefulWidget {
@@ -27,75 +28,140 @@ class _FooterDialogReCommentState extends State<FooterDialogReComment> {
   @override
   void initState() {
     super.initState();
+    final provider = Provider.of<ChatsProfiver>(context,listen: false);
+    provider.emojiShowing = false;
     myFocusNode.requestFocus();
   }
 
   @override
   Widget build(BuildContext context) {
     TextEditingController textEditingController = TextEditingController();
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      color: Colors.white,
-      padding: const EdgeInsets.all(10),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Row(
+    return Consumer<ChatsProfiver>(
+      builder: (context, provider, child) {
+        return Container(
+          width: MediaQuery.of(context).size.width,
+          color: Colors.white,
+          child: SingleChildScrollView(
+            child: Column(
               children: [
-                Expanded(
-                  child: Card(
-                    color: Colors.white70,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.only(
-                                left: 20, top: 5, bottom: 5),
-                            child: TextField(
-                              maxLines: null,
-                              focusNode: myFocusNode,
-                              controller: textEditingController,
-                              decoration: InputDecoration(
-                                hintText:
-                                    'Trả lời bình luận của ${widget.nameUserReComment}...',
-                                border: InputBorder.none,
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Card(
+                          color: Colors.white70,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 20, top: 5, bottom: 5),
+                                  child: TextField(
+                                    maxLines: null,
+                                    focusNode: myFocusNode,
+                                    controller: textEditingController,
+                                    decoration: InputDecoration(
+                                      hintText:
+                                      'Trả lời bình luận của ${widget.nameUserReComment}...',
+                                      border: InputBorder.none,
+                                    ),
+                                  ),
+                                ),
                               ),
-                            ),
+                              IconButton(
+                                onPressed: () {
+                                  if(provider.emojiShowing){
+                                    myFocusNode.requestFocus();
+                                  }else{
+                                    FocusScope.of(context).requestFocus(FocusNode());
+                                  }
+                                  provider.setEmojiShowing(!provider.emojiShowing);
+                                },
+                                icon: const Icon(Icons.emoji_emotions),
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  CommentService().sendReComment(
+                                      widget.videoId,
+                                      widget.idComment,
+                                      textEditingController.text.trim(),
+                                      widget.uId);
+                                  textEditingController.clear();
+                                  Navigator.pop(context);
+                                  // int index = videoProvider.listVideo
+                                  //     .indexWhere((element) => element == videoId);
+                                  // videoProvider.listVideo[index].comments.add('');
+                                },
+                                icon: Icon(Icons.send),
+                              ),
+                            ],
                           ),
                         ),
-                        IconButton(
-                          onPressed: () {
-                            // Handle emoji picker here.
-                          },
-                          icon: const Icon(Icons.emoji_emotions),
-                        ),
-                        IconButton(
-                          onPressed: () {
-                            CommentService().sendReComment(
-                                widget.videoId,
-                                widget.idComment,
-                                textEditingController.text.trim(),
-                                widget.uId);
-                            textEditingController.clear();
-                            Navigator.pop(context);
-                            // int index = videoProvider.listVideo
-                            //     .indexWhere((element) => element == videoId);
-                            // videoProvider.listVideo[index].comments.add('');
-                          },
-                          icon: Icon(Icons.send),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
+                Offstage(
+                  offstage: !provider.emojiShowing,
+                  child: SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.3,
+                    child: EmojiPicker(
+                      // onEmojiSelected: (Category category, Emoji emoji) {
+                      //   // Do something when emoji is tapped (optional)
+                      // },
+                      onBackspacePressed: () {
+                        // Do something when the user taps the backspace button (optional)
+                        // Set it to null to hide the Backspace-Button
+                      },
+                      textEditingController: textEditingController,
+                      // pass here the same [TextEditingController] that is connected to your input field, usually a [TextFormField]
+                      config: Config(
+                        columns: 7,
+                        emojiSizeMax: 32 *
+                            (foundation.defaultTargetPlatform ==
+                                TargetPlatform.iOS
+                                ? 1.30
+                                : 1.0),
+                        // Issue: https://github.com/flutter/flutter/issues/28894
+                        verticalSpacing: 0,
+                        horizontalSpacing: 0,
+                        gridPadding: EdgeInsets.zero,
+                        initCategory: Category.RECENT,
+                        bgColor: Color(0xFFF2F2F2),
+                        indicatorColor: Colors.blue,
+                        iconColor: Colors.grey,
+                        iconColorSelected: Colors.blue,
+                        backspaceColor: Colors.blue,
+                        skinToneDialogBgColor: Colors.white,
+                        skinToneIndicatorColor: Colors.grey,
+                        enableSkinTones: true,
+                        recentTabBehavior: RecentTabBehavior.RECENT,
+                        recentsLimit: 28,
+                        noRecents: const Text(
+                          'No Recents',
+                          style: TextStyle(
+                              fontSize: 20, color: Colors.black26),
+                          textAlign: TextAlign.center,
+                        ),
+                        // Needs to be const Widget
+                        loadingIndicator: const SizedBox.shrink(),
+                        // Needs to be const Widget
+                        tabIndicatorAnimDuration: kTabScrollDuration,
+                        categoryIcons: const CategoryIcons(),
+                        buttonMode: ButtonMode.MATERIAL,
+                      ),
+                    ),
+                  ),
+                )
               ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
